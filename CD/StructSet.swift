98bdf771f3c7267f32f396841f6436e6b67795cb
Muralidhargithub <ot41975@umbc.edu>
2025-01-20 15:16:47 -0500

    initial set up

---
+++ CD/StructSet.swift
@@ -0,0 +1,68 @@
+//
+//  StructSet.swift
+//  CD
+//
+//  Created by Muralidhar reddy Kakanuru on 1/20/25.
+//
+
+import Foundation
+
+
+struct StructSet<T: Comparable> {
+    var elements: [T] = []
+
+    func mergeSort(_ array: [T]) -> [T] {
+        guard array.count > 1 else { return array }
+        let middleIndex = array.count / 2
+        let leftArray = mergeSort(Array(array[..<middleIndex]))
+        let rightArray = mergeSort(Array(array[middleIndex...]))
+        return merge(leftArray, rightArray)
+    }
+
+    func merge(_ left: [T], _ right: [T]) -> [T] {
+        var leftIndex = 0
+        var rightIndex = 0
+        var sortedArray: [T] = []
+        while leftIndex < left.count && rightIndex < right.count {
+            if left[leftIndex] < right[rightIndex] {
+                sortedArray.append(left[leftIndex])
+                leftIndex += 1
+            } else {
+                sortedArray.append(right[rightIndex])
+                rightIndex += 1
+            }
+        }
+        if leftIndex < left.count { sortedArray.append(contentsOf: left[leftIndex...]) }
+        if rightIndex < right.count { sortedArray.append(contentsOf: right[rightIndex...]) }
+        return sortedArray
+    }
+
+    mutating func addElement(_ element: T) {
+        if !elements.contains(element) {
+            elements.append(element)
+            elements = mergeSort(elements)
+        }
+    }
+
+    mutating func addElements(_ arrElements: [T]) {
+        for i in arrElements { addElement(i) }
+    }
+
+    mutating func removeValue(_ value: T) {
+        if let findIndex = elements.firstIndex(of: value) {
+            elements.remove(at: findIndex)
+        }
+    }
+
+    mutating func removeValueAt(_ index: Int) {
+        elements.remove(at: index)
+    }
+
+    mutating func maxArrayEle() -> T? {
+        return elements.max()
+    }
+
+    mutating func minArrayEle() -> T? {
+        return elements.min()
+    }
+}
---
+++ CDTests/CDTests.swift
@@ -0,0 +1,68 @@
+//
+//  CDTests.swift
+//  CDTests
+//
+//  Created by Muralidhar reddy Kakanuru on 1/20/25.
+//
+
+import XCTest
+
+@testable import CD
+
+final class StructSetTests: XCTestCase {
+    var structSet: StructSet<Int>!
+
+    override func setUp() {
+        super.setUp()
+        structSet = StructSet()
+    }
+
+    override func tearDown() {
+        structSet = nil
+        super.tearDown()
+    }
+
+    func testEmptyArray() {
+        XCTAssertEqual(structSet.elements, [])
+    }
+
+    func testAddElement() {
+        structSet.addElement(1)
+        XCTAssertEqual(structSet.elements, [1])
+    }
+
+    func testAddElementsSort() {
+        structSet.addElements([1, 6, 4])
+        XCTAssertEqual(structSet.elements, [1, 4, 6])
+    }
+
+    func testDuplicates() {
+        structSet.addElement(9)
+        structSet.addElement(9)
+        XCTAssertEqual(structSet.elements, [9])
+    }
+
+    func testRemoveElementValue() {
+        structSet.addElements([1, 6, 4])
+        structSet.removeValue(6)
+        XCTAssertEqual(structSet.elements, [1, 4])
+    }
+
+    func testRemoveElementAt() {
+        structSet.addElements([1, 6, 4])
+        structSet.removeValueAt(2)
+        XCTAssertEqual(structSet.elements, [1, 4])
+    }
+
+    func testMaxEle() {
+        structSet.addElements([1, 6, 4])
+        let maxElement = structSet.maxArrayEle()
+        XCTAssertEqual(maxElement, 6)
+    }
+
+    func testMinEle() {
+        structSet.addElements([1, 6, 4])
+        let minElement = structSet.minArrayEle()
+        XCTAssertEqual(minElement, 1)
+    }
+}
